92a190148b96beaf4afc3c5181a1620c14822054
dbubel <50341559+dbubel@users.noreply.github.com>
2023-07-11 10:28:59 -0400

    fmt file

--- table.sql
+++ table.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS kinesis_shards
 (
     shard_id     text NOT NULL default '',
     in_use       bool NOT NULL default false,
-    last_seq    text not null default '',
+    last_seq     text not null default '',
     last_updated timestamp     default now(),
 --     expired bool not null default false,
     CONSTRAINT kinesis_shards_pk PRIMARY KEY (shard_id)
